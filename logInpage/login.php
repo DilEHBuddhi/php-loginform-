0c87b3e58942843679ab32f4787a10851c1a6dc7
Buddhi Dilshan <DilEHBuddhi@users.noreply.github.com>
2018-10-04 13:23:32 +0530

    added the project

---
+++ logInpage/login.php
@@ -0,0 +1,133 @@
+<?php
+
+$host ="localhost";
+$user ="root";
+$password ="";
+$dbname ="text";
+
+$con=mysqli_connect($host,$user,$password,$dbname);
+
+if(isset($_POST['username'])){
+  $uname = $_POST['username'];
+  $password = $_POST['password'];
+
+  $sql="select * from loginpage where Username='".$uname."'AND Password='".$password."' limit 1";
+
+  $result=mysqli_query($con, $sql);
+
+  if(mysqli_num_rows($result)==1){
+    echo "<h1> You Have Successfully Logged In </h1>";
+    exit();
+  }
+  else{
+    echo "You Have Entered Incorrect Password";
+    exit();
+  }
+}
+
+ ?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
+
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+button:hover {
+    opacity: 0.8;
+}
+
+.cancelbtn {
+    width: auto;
+    padding: 10px 18px;
+    background-color: #f44336;
+}
+
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+}
+
+img.avatar {
+    width: 20%;
+    border-radius: 30%;
+}
+
+.container {
+    padding: 16px;
+}
+
+span.psw {
+    float: right;
+    padding-top: 16px;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+    span.psw {
+       display: block;
+       float: none;
+    }
+    .cancelbtn {
+       width: 100%;
+    }
+    h2 {
+      text-align: center;
+    }
+}
+</style>
+</head>
+<body>
+
+<h2>Login Form</h2>
+
+<form method ="post" action="#">
+  <div class="imgcontainer">
+    <img src="imgLogin/img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="username" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password" required>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+
+</body>
+</html>
